1ad24a57f5ffb1cf382209b45eccdcb5c18841ea
Joel <jjalixtoc@gmail.com>
2024-02-02 14:11:00 -0500

    avance-2

--- Fronted-UserApp/src/pages/UsersPage.jsx
+++ Fronted-UserApp/src/pages/UsersPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { UsersList } from "./components/UsersList";
-import { useUsers } from "./hooks/useUsers";
-import { UserModalForm } from "./components/UserModalForm";
-
+import { UsersList } from "../components/UsersList";
+import { useUsers } from "../hooks/useUsers";
+import { UserModalForm } from "../components/UserModalForm";
     
-export const UsersApp = () => {
+export const UsersPage = () => {
 
     const {
         users,
